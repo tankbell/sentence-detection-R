2c16f2db28df1587c48a2591eb1a5988e2284e93
Gayathri and Ramesh <tankbell@Gayathris-MacBook-Pro.local>
2018-02-01 22:03:42 -0800

    framework

---
+++ app/OpenNLPSD.js
@@ -0,0 +1,9 @@
+var OpenNLPSentenceDetector = function() {};
+
+OpenNLPSentenceDetector.prototype.detect = function(alg,
+                                                    para,
+                                                    callback) {
+   //console.log("OSD " + alg);
+};
+
+exports.OpenNLPSentenceDetector = OpenNLPSentenceDetector;
--- app/routes.js
+++ app/routes.js
@@ -1,5 +1,32 @@
 module.exports = function(app) {
+  var OpenNLPSentenceDetector = require('./OpenNLPSD.js').OpenNLPSentenceDetector;
+  var openSD = new OpenNLPSentenceDetector();
+  //var openNLP = require("opennlp");
+
   app.get('/', function(req,res) {
     res.render('pages/sentDetect');
+  });
+
+  app.get('/cancel', function(req, res) {
+    res.redirect('/');
+  });
+
+  app.get('/goback', function(req, res) {
+    res.redirect('/');
+  });
+
+  app.post('/sentDetect', function(req, res) {
+    var alg  = req.body.algotype;
+    var para = req.body.msg;
+    //console.log(alg);
+    //console.log(para);
+    openSD.detect(alg, para, function(err, data) {
+      if (err) {
+        res.send("Server Error");
+      } else {
+        res.render('pages/output');
+      }
+    });
+
   });
 };
